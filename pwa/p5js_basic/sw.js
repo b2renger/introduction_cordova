149a44d89b85787a253089e9b3fbf9db28c336a6
Bérenger <berenger.recoules@gmail.com>
2020-07-08 11:02:17 +0200

    fix sw link to cached files

--- pwa/p5js_basic/sw.js
+++ pwa/p5js_basic/sw.js
@@ -1,27 +1,28 @@
 var cacheName = 'p5js-pwa';
+
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/sketch.js',
-  '/main.js',
-  '/libs/p5.min.js',
-  '/libs/p5.sound.js'
+    '/',
+    './index.html',
+    './sketch.js',
+    './main.js',
+    'libs/p5.min.js',
+    'libs/p5.sound.min.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
 });
